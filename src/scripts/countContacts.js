e44c32200dcc728c36595540d4033e8d398fb76c
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-05-31 20:20:01 +0300

    add scripts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,12 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+export const countContacts = async () => {
+    try{
+        const contacts = await readContacts();
+        return contacts.length;
+    } catch (error) {
+        console.error("Error counting contacts:", error.message);
+        return 0;
+    }
+};
 
 console.log(await countContacts());
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,18 @@
-const generateContacts = async (number) => {};
+import {createFakeContact} from '../utils/createFakeContact.js';
+import {writeContacts} from '../utils/writeContacts.js';
+import {readContacts} from '../utils/readContacts.js';
+
+
+const generateContacts = async (number) => {
+    try {
+        const readContactsData = await readContacts();
+        const newContacts = Array.from({ length: number }, () => createFakeContact());
+        const updateContacts = [...readContactsData, ...newContacts];
+        await writeContacts(updateContacts);
+        console.log(`Generated ${number} new contacts successfully.`);
+    } catch (error) {
+        console.error('Error generating contacts:', error.message);
+    }
+};
 
 generateContacts(5);
