8634b1a29e923b8c75a42ae693dbf160ec7dd145
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-05-31 13:50:52 +0300

    add read and write contacs

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,8 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    const data = await fs.readFile(path.resolve(PATH_DB), 'utf-8');
+    return JSON.parse(data);
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,8 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    await fs.writeFile(path.resolve(PATH_DB), JSON.stringify(updatedContacts, null, 2));
+    return writeContacts;
+};
